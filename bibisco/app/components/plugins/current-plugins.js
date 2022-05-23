0d4221e109eae8b9a81b1fa67628a62003ca7818
Shannon Huggins <phate@houseofsyn.com>
2022-05-24 08:22:22 +1000

    - Initial start on plugins system NOTE: NOT IMPLEMENTED.

---
+++ bibisco/app/components/plugins/current-plugins.js
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2014-2021 Andrea Feccomandi
+ *
+ * Licensed under the terms of GNU GPL License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY.
+ * See the GNU General Public License for more details.
+ *
+ */
+angular.
+  module('bibiscoApp').
+  component('openproject', {
+    templateUrl: 'components/project/open-project.html',
+    controller: OpenProjectController
+  });
+
+function OpenProjectController($location, $rootScope, $timeout, $translate, ChapterService,
+  LoggerService, PopupBoxesService, ProjectService) {
+
+  // hide menu
+  $rootScope.$emit('SHOW_OPEN_PROJECT');
+
+  var self = this;
+  self.$onInit = function () {
+    self.confirmdialogopen = false;
+    self.hotkeys = ['esc'];
+    self.projects = ProjectService.getProjects();
+  };
+
+  self.open = function(id) {
+
+    // load project
+    ProjectService.load(id);
+   
+    // check words written
+    ChapterService.checkWordsWrittenInit();
+    LoggerService.info('Open project ' + id);
+
+    // reset partsExpansionStatus
+    $rootScope.partsExpansionStatus = [];
+
+    // go to project home
+    $location.path('/projecthome');
+  };
+
+  self.delete = function (id, projectName) {
+    let message = $translate.instant('jsp.selectProject.delete.confirm', { projectName: projectName });
+    PopupBoxesService.confirm(function () {
+      $timeout(function () {
+        ProjectService.delete(id);
+        self.projects = ProjectService.getProjects();
+      }, 0);
+    },
+    message,
+    function () {
+
+    });
+  };
+
+  self.back = function() {
+    $location.path('/start');
+  };
+}
---
+++ bibisco/app/components/plugins/import-plugin.js
@@ -0,0 +1,122 @@
+/*
+ * Copyright (C) 2014-2021 Andrea Feccomandi
+ *
+ * Licensed under the terms of GNU GPL License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY.
+ * See the GNU General Public License for more details.
+ *
+ */
+angular.
+  module('bibiscoApp').
+  component('importproject', {
+    templateUrl: 'components/project/import-project.html',
+    controller: ImportProjectController
+  });
+
+function ImportProjectController($location, $rootScope, $scope, $timeout, 
+  $translate, hotkeys, PopupBoxesService, ProjectService) {
+
+  // hide menu
+  $rootScope.$emit('SHOW_IMPORT_PROJECT');
+
+  var self = this;
+  self.fileToImport = null;
+  self.invalidArchive = false;
+  self.projectName;
+  self.projectId;
+
+  self.$onInit = function () {
+    hotkeys.bindTo($scope)
+      .add({
+        combo: ['esc', 'esc'],
+        description: 'esc',
+        callback: function ($event) {
+          $event.preventDefault();
+          setTimeout(function () {
+            document.getElementById('importProjectBackButton').focus();
+            document.getElementById('importProjectBackButton').click();
+          }, 0);
+        }
+      });
+
+    self.checkExit = {
+      active: true
+    };
+    self.backpath = '/start';
+  };
+
+
+  self.selectFileToImport = function(file) {
+    self.fileToImport = file;
+    self.invalidArchive = false;
+    self.projectName = null;
+    self.projectId = null;
+    $scope.$apply();
+  };
+
+  self.confirmImportExistingProject = function() {
+    ProjectService.importExistingProject(self.projectId, self.projectName,
+      function() {
+        $location.path('/projecthome');
+      });
+  };
+
+  self.save = function(isValid) {
+    if (!isValid || self.invalidArchive) {
+      return;
+    }
+
+    self.checkExit = {
+      active: false
+    };
+    ProjectService.importProjectArchiveFile(self.fileToImport, function(
+      result) {
+
+      // is valid archive and the project isn't present in bibisco installation
+      if (result.isValidArchive && !result.isAlreadyPresent) {
+        ProjectService.import(result.projectId, result.projectName,
+          function() {
+            $location.path('/projecthome');
+            $scope.$apply(); // Why?!? http://stackoverflow.com/questions/11784656/angularjs-location-not-changing-the-path
+          });
+
+      // is valid archive and the project is present in bibisco installation
+      } else if (result.isValidArchive && result.isAlreadyPresent) {
+        self.invalidArchive = false;        
+        $scope.$apply();
+
+        self.projectName = result.projectName;
+        self.projectId = result.projectId;
+        let message = $translate.instant('jsp.closeImportProject.importAlreadyPresent.confirm', { projectName: self.projectName });
+        PopupBoxesService.confirm(function () {
+          $timeout(function () {
+            self.confirmImportExistingProject();
+          }, 0);
+        },
+        message,
+        function () {
+          self.projectName = null;
+          self.projectId = null;
+        });
+
+      // is not valid archive
+      } else {
+        self.invalidArchive = true;
+        self.projectName = null;
+        self.projectId = null;
+        $scope.$apply();
+      }
+
+    });
+  };
+
+  $scope.$on('$locationChangeStart', function (event) {
+    PopupBoxesService.locationChangeConfirm(event, self.fileToImport, self.checkExit);
+  });
+}
